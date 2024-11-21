389522a2790b3fb49872279da3b946ab947aadcd
Erik Jensen <365211+erikrj@users.noreply.github.com>
2024-11-20 19:47:12 -0700

    Fixing app build issues

--- app/src/entry-aws-lambda.tsx
+++ app/src/entry-aws-lambda.tsx
@@ -7,22 +7,22 @@
  * - https://qwik.dev/docs/deployments/aws/
  *
  */
-import "source-map-support/register";
-import serverless from "serverless-http";
+import 'source-map-support/register';
+import serverless from 'serverless-http';
 import {
   createQwikCity,
   type PlatformAwsLambda,
-} from "@builder.io/qwik-city/middleware/aws-lambda";
-import qwikCityPlan from "@qwik-city-plan";
-import { manifest } from "@qwik-client-manifest";
-import render from "./entry.ssr";
+} from '@builder.io/qwik-city/middleware/aws-lambda';
+import qwikCityPlan from '@qwik-city-plan';
+import {manifest} from '@qwik-client-manifest';
+import render from './entry.ssr';
 
 declare global {
-  interface QwikCityPlatform extends PlatformAwsLambda {}
+  type QwikCityPlatform = PlatformAwsLambda;
 }
 
-export const { handle } = createQwikCity({ render, qwikCityPlan, manifest });
+export const {handle} = createQwikCity({render, qwikCityPlan, manifest});
 
-export const qwikApp = serverless({ handle }, { binary: true });
+export const qwikApp = serverless({handle}, {binary: true});
 // handler is the default export for the lambda functions
 export const handler = qwikApp;
